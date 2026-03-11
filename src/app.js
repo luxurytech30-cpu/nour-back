d99cea64c5926737b63b9242d5e93965b19292ee
Helal Ali <helalali358@gmail.com>
2026-03-11 19:07:17 +0200

    a

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const servicesRoutes = require("./routes/services.routes");
 const app = express();
 
 const allowedOrigins = [
-  "https://epex-ruby.vercel.app/",
+  "https://epex-ruby.vercel.app",
   "http://localhost:8080",
   "http://localhost:8081",
 ];
