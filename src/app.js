c9c8e5e3583f0628077fc4e8ac121f3bdcd8faef
luxurytech30 <luxurytech30@gmail.com>
2026-03-12 23:37:47 +0200

    asd

--- src/app.js
+++ src/app.js
@@ -28,6 +28,29 @@ const allowedOrigins = [
 
 app.use(
   cors({
+    origin(origin, callback) {
+      console.log("CORS ORIGIN:", origin);
+
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error(`Origin not allowed: ${origin}`));
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  }),
+);
+app.use((req, res, next) => {
+  console.log("ORIGIN:", req.headers.origin);
+  console.log("COOKIE:", req.headers.cookie);
+  next();
+});
+
+app.use(
+  cors({
     origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
