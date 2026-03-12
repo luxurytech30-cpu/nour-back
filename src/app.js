1fa160fcd26777a6263bc0f4bb92c0e012274303
luxurytech30 <luxurytech30@gmail.com>
2026-03-12 23:43:44 +0200

    asd

--- src/app.js
+++ src/app.js
@@ -20,11 +20,13 @@ const app = express();
 
 const allowedOrigins = [
   "https://epex-ruby.vercel.app",
+  "https://apex-nine-mu.vercel.app",
   "http://localhost:8080",
   "http://localhost:8081",
-  "https://apex-nine-mu.vercel.app",
   "http://192.168.1.7:8080",
 ];
+
+app.set("trust proxy", 1);
 
 app.use(
   cors({
@@ -43,24 +45,15 @@ app.use(
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   }),
 );
+
 app.use((req, res, next) => {
   console.log("ORIGIN:", req.headers.origin);
   console.log("COOKIE:", req.headers.cookie);
   next();
 });
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  }),
-);
-
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
-
-app.set("trust proxy", 1);
 
 app.use(
   session({
@@ -75,8 +68,8 @@ app.use(
     }),
     cookie: {
       httpOnly: true,
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      secure: false,
       maxAge: 1000 * 60 * 60 * 24 * 7,
     },
   }),
@@ -97,4 +90,5 @@ app.use("/api/availability", require("./routes/availability.routes"));
 app.use("/api/services", servicesRoutes);
 app.use("/api/waitlist", waitlistRoutes);
 app.use("/api/verify", verfyRoutes);
+
 module.exports = app;
